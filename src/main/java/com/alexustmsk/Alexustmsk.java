31a7feaefaf531f63ed2d3c70b93c628d7a51a2a
Alexander Ustyuzhanin <alexustmsk@gmail.com>
2018-05-08 23:46:52 +0300

    Корректный манифест и обработка старта без параметров

--- src/main/java/com/alexustmsk/Alexustmsk.java
+++ src/main/java/com/alexustmsk/Alexustmsk.java
@@ -1,9 +1,21 @@
 package com.alexustmsk;
 
-import com.alexustmsk.ws.*;
+import com.alexustmsk.ws.FNSNDSCAWS2;
+import com.alexustmsk.ws.NdsRequest2;
+import com.alexustmsk.ws.NdsResponse2;
+import com.alexustmsk.ws.ObjectFactory;
+
+import javax.xml.namespace.QName;
+import java.io.IOException;
+import java.net.URL;
 
 public class Alexustmsk {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            System.out.println("Укажите ИНН");
+            return;
+        }
 
         ObjectFactory objectFactory = new ObjectFactory();
 
@@ -15,20 +27,14 @@ public class Alexustmsk {
             ndsRequest2.getNP().add(ndsRequest2NP);
         }
 
-        FNSNDSCAWS2 fnsndscaws2 = new FNSNDSCAWS2();
+        FNSNDSCAWS2 fnsndscaws2 = new FNSNDSCAWS2(new URL("http://npchk.nalog.ru/FNSNDSCAWS_2?wsdl"), new QName("http://ws.unisoft", "FNSNDSCAWS2"));
         NdsResponse2 ndsResponse2 = fnsndscaws2.getFNSNDSCAWS2Port().ndsRequest2(ndsRequest2);
 
         for (NdsResponse2.NP np : ndsResponse2.getNP()) {
             System.out.println(np.getState());
         }
-
-
-
-
-
-        }
-
     }
+}
 
 
 
